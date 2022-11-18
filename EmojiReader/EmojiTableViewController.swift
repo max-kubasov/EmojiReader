770a44f02d7646516890d3c550a57c4cb8cf02d0
max-kubasov <kubasov87@gmail.com>
2022-11-18 20:23:30 +0200

    Add delete and move cells

--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
-    let objects = [
+    var objects = [
     Emoji(emoji: "😉", name: "Hello", description: "Say hello to all friends", isFavourite: false),
     Emoji(emoji: "⚽️", name: "Footboll", description: "Let's play football together", isFavourite: false),
     Emoji(emoji: "🥰", name: "Love", description: "Let's love each other", isFavourite: false)
@@ -48,6 +48,28 @@ class EmojiTableViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath ], with: .fade)
+        } else {
+            print("321")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedEmoji = objects.remove(at: sourceIndexPath.row)
+        objects.insert(movedEmoji, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
