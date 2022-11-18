efa78e030d6223220ec1b664affa7b581b68ed7c
max-kubasov <kubasov87@gmail.com>
2022-11-18 19:56:32 +0200

    Add table view data model

---
+++ EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Max on 18.11.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
--- EmojiTableViewCell.swift
+++ EmojiTableViewCell.swift
@@ -17,5 +17,11 @@ class EmojiTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    func set(object: Emoji) {
+        self.emojiLabel.text = object.emoji
+        self.nameLabel.text = object.name
+        self.descriptionLabel.text = object.description
+    }
 
 }
