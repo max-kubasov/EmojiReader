86c7805293eb0c23c331c7c4ed27259fa0ab28ca
max-kubasov <kubasov87@gmail.com>
2022-11-19 16:41:54 +0200

    Add new cell function

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
+    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
 
     @IBOutlet weak var emojiTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
@@ -17,10 +19,32 @@ class NewEmojiTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateSaveButton()
 
     }
-
-    @IBAction func textChanged(_ sender: Any) {
+    
+    private func updateSaveButton() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
     }
+
+    @IBAction func textChanged(_ sender: UITextField) {
+        updateSaveButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "saveSegue" else { return }
+        
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
+    }
+    
+    
 
 }
